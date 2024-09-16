38ee50a3bd0093bf3ccb7ad5c37cf4653c6e1f28
jo4ko021 <jo4ko021@gmail.com>
2024-09-16 10:47:51 -0300

    creamos y modificamos metodos para las imagenes y sus URLs dentro de crud service y table component ts

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -10,6 +10,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class TableComponent {
 
+  nombreimagen!: string
+  imagen!: string
+
   productocollection: Producto[] = []
   productoseleccionado!: Producto
   modalvisibleproducto: boolean = false
@@ -19,7 +22,7 @@ export class TableComponent {
     precio: new FormControl(0, Validators.required),
     categoria: new FormControl('', Validators.required),
     descripcion: new FormControl('', Validators.required),
-    imagen: new FormControl('', Validators.required),
+    //imagen: new FormControl('', Validators.required),
     alt: new FormControl('', Validators.required)
   })
 
@@ -39,22 +42,39 @@ export class TableComponent {
         precio: this.producto.value.precio!,
         categoria: this.producto.value.categoria!,
         descripcion: this.producto.value.descripcion!,
-        imagen: this.producto.value.imagen!,
+        imagen: '',
         alt: this.producto.value.alt!
 
       }
-
-
-      await this.serviciocrud.crearProducto(nuevoproducto)
-        .then(producto => {
-          alert("Ha agregado un nuevo producto con éxito.");
-          // this.producto.reset();
+      await this.serviciocrud.subirimagen(this.nombreimagen, this.imagen, 'productos')
+        .then(resp => {
+          this.serviciocrud.obtenerurlimagen(resp)
+            .then(url => {
+              this.serviciocrud.crearProducto(nuevoproducto, url)
+                .then(producto => {
+                  alert("Ha agregado un nuevo producto con éxito.");
+                  // this.producto.reset();
+                })
+                .catch(error => {
+                  alert("Ha ocurrido un error al cargar un producto.");
+                  // this.producto.reset();
+                })
+            })
         })
-        .catch(error => {
-          alert("Ha ocurrido un error al cargar un producto.");
-          // this.producto.reset();
-        })
-
+    }
+  }
+  cargarimagenes(event:any) {
+    let archivo = event.target.files[0]
+    let reader = new FileReader()
+    if(archivo!=undefined){
+      reader.readAsDataURL(archivo)
+      reader.onloadend = () => {
+        const url = reader.result
+        if(url!=null){
+          this.nombreimagen = archivo.name
+          this.imagen = url.toString()
+        }
+      }
     }
 
   }
@@ -69,38 +89,38 @@ export class TableComponent {
         alert("Se ha podido eliminar con éxito.");
       })
       .catch(error => {
-        alert("Ha ocurrido un error al eliminar un producto: \n"+error);
+        alert("Ha ocurrido un error al eliminar un producto: \n" + error);
       })
   }
 
-  mostrarEditar(productoseleccionado: Producto){
+  mostrarEditar(productoseleccionado: Producto) {
     this.producto.setValue({
-    nombre: productoseleccionado.nombre,
-    precio: productoseleccionado.precio,
-    descripcion: productoseleccionado.descripcion,
-    categoria: productoseleccionado.categoria,
-    imagen: productoseleccionado.imagen,
-    alt: productoseleccionado.alt
-  })
+      nombre: productoseleccionado.nombre,
+      precio: productoseleccionado.precio,
+      descripcion: productoseleccionado.descripcion,
+      categoria: productoseleccionado.categoria,
+      // imagen: productoseleccionado.imagen,
+      alt: productoseleccionado.alt
+    })
   }
 
-  editarProductos(){
-    let datos : Producto = {
+  editarProductos() {
+    let datos: Producto = {
       idproducto: this.productoseleccionado.idproducto,
       nombre: this.producto.value.nombre!,
       precio: this.producto.value.precio!,
       descripcion: this.producto.value.descripcion!,
       categoria: this.producto.value.categoria!,
-      imagen: this.producto.value.imagen!,
+      imagen: '',
       alt: this.producto.value.alt!
     }
     this.serviciocrud.modificarProducto(this.productoseleccionado.idproducto, datos)
-    .then(producto => {
-      alert("¡El producto se ha modificado con exito!")
-    })
-    .catch(error =>{
-      alert("¡Hubo un error al modificar el producto! "+error)
-    })
+      .then(producto => {
+        alert("¡El producto se ha modificado con exito!")
+      })
+      .catch(error => {
+        alert("¡Hubo un error al modificar el producto! " + error)
+      })
   }
 
 }
--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+import { getDownloadURL, getStorage, ref, UploadResult, uploadString, deleteObject } from 'firebase/storage'
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
+  private respuesta!: UploadResult
+  private storage = getStorage()
   private productocollection: AngularFirestoreCollection<Producto>
   constructor(private database: AngularFirestore) {
     this.productocollection = database.collection('producto')
   }
-  crearProducto(producto: Producto) {
+  crearProducto(producto: Producto, url: string) {
     return new Promise(async (resolve, reject) => {
       try {
+        producto.imagen = url
         const idproducto = this.database.createId()
         producto.idproducto = idproducto
         const resultado = await this.productocollection.doc(idproducto).set(producto);
@@ -26,17 +30,17 @@ export class CrudService {
     }
     )
   }
-  obtenerProducto(){
+  obtenerProducto() {
     /*
     snapshotchanges captura el estado de los datos del sistema
     las pipe son tuberias que retornan un nuevo array
     map mapea o recorre esa nueva informacion
     a resguarda la nueva informacion y la envia como un documento
     */
-    return this.productocollection.snapshotChanges().pipe(map(Action=>Action.map(a=>a.payload.doc.data())))
+    return this.productocollection.snapshotChanges().pipe(map(Action => Action.map(a => a.payload.doc.data())))
   }
   // EDITAR productos
-  modificarProducto(idProducto: string, nuevaData: Producto){
+  modificarProducto(idProducto: string, nuevaData: Producto) {
     /*
       Accedemos a la colección "productos" de la Base de Datos, buscamos el ID del 
       producto seleccionado y lo actualizamos con el método "update", enviando la 
@@ -46,16 +50,51 @@ export class CrudService {
   }
 
   // ELIMINAR productos
-  eliminarProducto(idProducto: string){
+  eliminarProducto(idProducto: string, imagenURL: string) {
     return new Promise((resolve, reject) => {
-      try{
-        const respuesta = this.productocollection.doc(idProducto).delete();
+      try {
+        const storage = getStorage()
 
-        resolve (respuesta);
+        const referenciaImagen = ref(storage, imagenURL)
+
+        deleteObject(referenciaImagen)
+          .then((resp) => {
+            const respuesta = this.productocollection.doc(idProducto).delete();
+
+            resolve(respuesta);
+          })
+          .catch(error => {
+            reject("error al eliminar la imagen:" + error)
+          })
+
       }
-      catch(error){
-        reject (error);
+      catch (error) {
+        reject(error);
       }
     })
+  }
+  obtenerurlimagen(respuesta: UploadResult) {
+    return getDownloadURL(respuesta.ref)
+  }
+  /**
+   * @param {string} nombre
+   * @param {any} imagen
+   * @param {string} ruta
+   * @returns
+   */
+  async subirimagen(nombre: string, imagen: any, ruta: string) {
+    try {
+      let referenciaImagen = ref(this.storage, ruta + '/' + nombre)
+      this.respuesta = await uploadString(referenciaImagen, imagen, 'data_url')
+        .then(resp => {
+          return resp
+        })
+
+        return this.respuesta
+    }
+    catch (error) {
+      console.log(error)
+      return this.respuesta
+    }
   }
 }
